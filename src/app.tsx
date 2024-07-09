8be260a1f6583261719cfe05fb400f0aa88c5774
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-07-09 08:51:47 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,25 @@
+
+import "./index.css"
+
+export function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="max-w-[720px] px-6 text-center">
+        <p className="text-zinc-300 text-lg ">
+          Convide seus amigos e planeje sua próxima viagem!
+        </p>
+
+        <div>
+          
+        </div>
+
+        <p>
+          Ao planejar sua viagem pela plann.er você automaticamente concorda
+          com nossos <a href="#">termos de uso</a> e <a href="#">políticas de privacidade.</a>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+28min
